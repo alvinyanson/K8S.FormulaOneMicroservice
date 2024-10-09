e3685762f777135df2197a9d19828c1d6a36f824
alvinyanson <a.yanson@arcanys.com>
2024-10-09 16:23:53 +0800

    feat: driver stats controller

--- K8S.DriverStatAPI/Controllers/DriverStatsController.cs
+++ K8S.DriverStatAPI/Controllers/DriverStatsController.cs
@@ -37,5 +37,56 @@ namespace K8S.DriverStatAPI.Controllers
                 return BadRequest("Http test connection failed on DriverStatAPI");
             }
         }
+
+        [HttpGet("GetByRaceWins")]
+        public async Task<IActionResult> GetByRaceWins()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync($"{_configuration.GetConnectionString("DriverAPI")}/GetTopDriversByRaceWins");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                    return Ok(jsonResponse);
+                }
+
+                return BadRequest("Http test connection failed on DriverStatAPI");
+            }
+        }
+
+        [HttpGet("GetByFastestLap")]
+        public async Task<IActionResult> GetByFastestLap()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync($"{_configuration.GetConnectionString("DriverAPI")}/GetTopDriversByFastestLap");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                    return Ok(jsonResponse);
+                }
+
+                return BadRequest("Http test connection failed on DriverStatAPI");
+            }
+        }
+
+        [HttpGet("GetByPolePosition")]
+        public async Task<IActionResult> GetByPolePosition()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync($"{_configuration.GetConnectionString("DriverAPI")}/GetTopDriversByPolePosition");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                    return Ok(jsonResponse);
+                }
+
+                return BadRequest("Http test connection failed on DriverStatAPI");
+            }
+        }
     }
 }
