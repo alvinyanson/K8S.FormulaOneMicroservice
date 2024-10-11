9daa5b9569af7677d6792254f0840ebc02fb20c9
alvinyanson <a.yanson@arcanys.com>
2024-10-11 17:19:26 +0800

    feat: ocelot api gateway load balance, caching and rate limiting

--- K8S.API.Gateway/Program.cs
+++ K8S.API.Gateway/Program.cs
@@ -1,3 +1,4 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -12,7 +13,7 @@ else
     builder.Configuration.AddJsonFile("ocelot.development.json", optional: false, reloadOnChange: true);
 }
 
-builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddOcelot(builder.Configuration).AddCacheManager(x => x.WithDictionaryHandle());
 
 var app = builder.Build();
 
